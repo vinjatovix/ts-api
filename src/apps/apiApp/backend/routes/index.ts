9f7e26fbf0b2e8b755e3bca29a199753b3d08409
Pablo Viña <111048414+pablojvritx@users.noreply.github.com>
2023-10-01 23:49:11 +0200

    feat(service): initialize api and add health endpoint (#1)

---
+++ src/apps/apiApp/backend/routes/index.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { globSync } from 'glob';
+
+export function registerRoutes(router: Router) {
+  const routes = globSync('**/*.routes.*', {
+    cwd: __dirname,
+    ignore: '**/index.ts'
+  });
+  routes.forEach((route) => {
+    _register(route, router);
+  });
+}
+
+const _register = (routePath: string, router: Router) => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const route = require(__dirname + '/' + routePath);
+  route.register(router);
+};
