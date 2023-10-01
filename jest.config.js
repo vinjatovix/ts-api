f17d90c675e47b1b151377073ec48c05f951c9cb
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2023-10-02 01:16:02 +0200

    wtffff

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	cacheDirectory: '.tmp/jestCache',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  cacheDirectory: '.tmp/jestCache',
   collectCoverage: true,
   coverageDirectory: '<rootDir>/coverage',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  watchPathIgnorePatterns: ["<rootDir>/test-report.json"],
-  reporters: ["default"],
-  testResultsProcessor: "jest-sonar-reporter",
-  coverageReporters: ["text", "html", "lcov", "clover"],
-  coveragePathIgnorePatterns: ["/node_modules/", "/test/"]
+  watchPathIgnorePatterns: ['<rootDir>/test-report.json'],
+  reporters: ['default'],
+  testResultsProcessor: 'jest-sonar-reporter',
+  coverageReporters: ['text', 'html', 'lcov', 'clover'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/']
 };
