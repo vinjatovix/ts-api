4e9f268abb2f6371e9b88cfe52e64b08cbe276f1
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2023-10-08 16:05:26 +0200

    feat(chore): setupb fl0 build

--- src/apps/apiApp/backend/start.ts
+++ src/apps/apiApp/backend/start.ts
@@ -1,13 +1,8 @@
 import dotenv from 'dotenv';
-import path from 'path';
+
 import { ApiApp } from './ApiApp';
 
-const envPath = path.resolve(__dirname, `./config/${process.env.NODE_ENV}.env`);
-
-console.log(envPath);
-dotenv.config({
-  path: envPath
-});
+dotenv.config();
 
 try {
   new ApiApp().start();
