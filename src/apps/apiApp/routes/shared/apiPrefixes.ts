9f30ef45caab28044899316f06e5111b3172647e
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2024-06-02 03:43:04 +0200

    [GH-148] refactor: add shared apiPrefixes

---
+++ src/apps/apiApp/routes/shared/apiPrefixes.ts
@@ -0,0 +1,9 @@
+import { KeyStringObject } from '../../shared/interfaces/KeyStringObject';
+
+export const API_PREFIXES: KeyStringObject = {
+  auth: '/api/v1/Auth',
+  author: '/api/v1/Authors',
+  book: '/api/v1/Books',
+  docs: '/api/v1/Docs',
+  health: '/api/v1/Health'
+};
