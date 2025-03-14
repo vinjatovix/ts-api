7333629ad0f6a3eb40281560404558fc738b41cf
vinjatovix <vinjatovix@gmail.com>
2025-03-14 04:08:07 +0000

    [GH-212] feat(infra): add mongo fetch service

--- src/Contexts/shared/infrastructure/persistence/mongo/MongoRepository.ts
+++ src/Contexts/shared/infrastructure/persistence/mongo/MongoRepository.ts
@@ -1,7 +1,11 @@
-import { Collection, MongoClient, ObjectId } from 'mongodb';
+import { Collection, MongoClient, MongoServerError, ObjectId } from 'mongodb';
+import { RequestOptions } from '../../../../../apps/apiApp/shared/interfaces';
 import { AggregateRoot } from '../../../domain/AggregateRoot';
 import { Username } from '../../../../apiApp/Auth/domain';
 import { updateMetadata } from '../../../application/utils';
+import { MongoErrorHandler } from './MongoErrorHandler';
+import { MongoFetchService } from './MongoFetchService';
+import { Entity } from './Entity';
 
 export abstract class MongoRepository<T extends AggregateRoot> {
   constructor(private readonly _client: Promise<MongoClient>) {}
@@ -39,5 +43,27 @@ export abstract class MongoRepository<T extends AggregateRoot> {
   protected async delete(id: string): Promise<void> {
     const collection = await this.collection();
     await collection.deleteOne({ _id: id as unknown as ObjectId });
+  }
+
+  protected async handleMongoError<T>(operation: () => Promise<T>): Promise<T> {
+    try {
+      return await operation();
+    } catch (err: unknown) {
+      if (err instanceof MongoServerError) {
+        MongoErrorHandler.formatError(err);
+      }
+      throw err;
+    }
+  }
+
+  protected async fetch<T extends Entity>({
+    id,
+    options
+  }: {
+    id?: string;
+    options: Partial<RequestOptions>;
+  }): Promise<T[]> {
+    const collection = await this.collection();
+    return MongoFetchService.fetch<T>({ collection, id, options });
   }
 }
