0f196d2c661b606a793938b77ba087e30f315d4e
vinjatovix <vinjatovix@gmail.com>
2025-03-21 21:59:00 +0000

    [GH-234] refactor(book): refactor domain

--- src/Contexts/apiApp/Books/domain/PopulatedBook.ts
+++ src/Contexts/apiApp/Books/domain/PopulatedBook.ts
@@ -1,12 +1,7 @@
 import { createError } from '../../../shared/domain/errors';
 import { Nullable } from '../../../shared/domain/types';
-import { Metadata, Uuid } from '../../../shared/domain/valueObject';
 import { Author } from '../../Authors/domain';
 import { BookBase } from './BookBase';
-import { BookPages } from './BookPages';
-import { BookReleaseDate } from './BookReleaseDate';
-import { BookTitle } from './BookTitle';
-import { Isbn } from './ISBN';
 import { BookPrimitives, PopulatedBookProps } from './interfaces';
 
 export class PopulatedBook extends BookBase {
@@ -24,33 +19,15 @@ export class PopulatedBook extends BookBase {
     };
   }
 
-  static fromPrimitives({
-    id,
-    metadata,
-    title,
-    author,
-    isbn,
-    releaseDate,
-    pages
-  }: BookPrimitives) {
+  static fromPrimitives({ author, ...primitives }: BookPrimitives) {
     if (!author || typeof author === 'string') {
       throw createError.invalidArgument(
         `Cannot create a populated book without a valid author`
       );
     }
-
     return new PopulatedBook({
-      id: new Uuid(id),
-      metadata: Metadata.fromPrimitives(metadata),
-      title: title ? new BookTitle(title) : undefined,
-      isbn: isbn ? new Isbn(isbn) : undefined,
-      releaseDate: releaseDate ? new BookReleaseDate(releaseDate) : undefined,
-      pages: pages ? new BookPages(pages) : undefined,
-      author: Author.fromPrimitives({
-        id: author.id,
-        name: author.name ?? undefined,
-        metadata: author.metadata
-      })
+      ...super.fromPrimitives(primitives),
+      author: Author.fromPrimitives(author)
     });
   }
 }
