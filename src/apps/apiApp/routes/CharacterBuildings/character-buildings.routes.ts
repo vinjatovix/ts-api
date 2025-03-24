da8c6562044a91d2de6ffac92aca8cc546fb38c2
vinjatovix <vinjatovix@gmail.com>
2025-03-24 02:04:08 +0000

    [GH-210] feat(SceneRemover): enhance scene removal logic to validate associated character buildings

--- src/apps/apiApp/routes/CharacterBuildings/character-buildings.routes.ts
+++ src/apps/apiApp/routes/CharacterBuildings/character-buildings.routes.ts
@@ -1,15 +1,20 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { API_PREFIXES } from '../shared';
-import { PostCharacterBuildingController } from '../../controllers/CharacterBuildings';
 import { auth, validateBody, validateReqSchema } from '../shared/middlewares';
 import container from '../../dependency-injection';
 import { postReqSchema } from './reqSchemas';
+import { PostController } from '../../controllers/shared/PostController';
+import { CharacterBuildingCreator } from '../../../../Contexts/apiApp/CharacterBuildings/application/CharacterBuildingCreator';
+import { CharacterBuildingCreatorRequest } from '../../../../Contexts/apiApp/CharacterBuildings/application/interfaces';
 
 const prefix = API_PREFIXES.characterBuilding;
 
 export const register = (router: Router) => {
-  const postController = container.get(
-    PostCharacterBuildingController.containerId
+  const postController: PostController<
+    CharacterBuildingCreator,
+    CharacterBuildingCreatorRequest
+  > = container.get(
+    'Apps.apiApp.controllers.CharacterBuildings.PostCharacterBuildingController'
   );
 
   router.post(
