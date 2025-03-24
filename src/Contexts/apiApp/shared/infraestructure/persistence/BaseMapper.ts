28e921b0df3a61467464daec73aefe0b4bedb7e4
vinjatovix <vinjatovix@gmail.com>
2025-03-24 17:30:02 +0000

    [GH-183] feat(BaseMapper): refactores base mapper

--- src/Contexts/apiApp/shared/infraestructure/persistence/BaseMapper.ts
+++ src/Contexts/apiApp/shared/infraestructure/persistence/BaseMapper.ts
@@ -1,34 +1,30 @@
+import { Entity } from '../../../../shared/infrastructure/persistence/mongo';
+
 export class BaseMapper {
-  // Mapper genérico para transformar '_id' a 'id'
-  static mapId<T extends { _id: string | number }>(document: T): T {
+  static mapId<T extends Entity>(document: T): T {
     const { _id, ...rest } = document;
     return { ...rest, id: _id } as unknown as T;
   }
 
-  // General method to handle nested objects and avoid repeating the logic
-  static mapNestedId<
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    T extends { _id?: string | number } & { [key: string]: any }
-  >(document: T): T {
-    if (document && typeof document === 'object') {
-      if ('_id' in document) {
-        if (document._id !== undefined) {
-          return this.mapId(
-            document as { _id: string | number }
-          ) as unknown as T;
-        }
-      }
-
-      // Recurse for nested objects (if needed)
-      Object.keys(document).forEach((key) => {
-        if (document[key] && typeof document[key] === 'object') {
-          (document as Record<string, unknown>)[key] = this.mapNestedId(
-            document[key]
-          );
-        }
-      });
+  static mapNestedId<T extends Entity>(document: T): T {
+    let newDocument = JSON.parse(JSON.stringify(document));
+    if ('_id' in newDocument) {
+      newDocument = this.mapId(newDocument);
     }
 
-    return document;
+    for (const key in newDocument) {
+      if (key === 'metadata') {
+        continue;
+      }
+      const value = newDocument[key];
+
+      if (Array.isArray(value)) {
+        newDocument[key] = value.map((item) => this.mapNestedId(item));
+      } else if (value && typeof value === 'object') {
+        newDocument[key] = this.mapNestedId(value);
+      }
+    }
+
+    return newDocument;
   }
 }
