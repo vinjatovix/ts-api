823a2bf53802350423f0ff62dd21833c8d1b092d
vinjatovix <vinjatovix@gmail.com>
2025-03-24 08:16:55 +0000

    [GH-210] feat(CharacterBuildings): implement CharacterBuildingPatcher and associated request interface for patching character buildings

--- tests/Contexts/apiApp/CharacterBuildings/__mocks__/CharacterBuildingRepositoryMock.ts
+++ tests/Contexts/apiApp/CharacterBuildings/__mocks__/CharacterBuildingRepositoryMock.ts
@@ -1,4 +1,6 @@
+import { Username } from '../../../../../src/Contexts/apiApp/Auth/domain';
 import { CharacterBuilding } from '../../../../../src/Contexts/apiApp/CharacterBuildings/domain';
+import { CharacterBuildingPatch } from '../../../../../src/Contexts/apiApp/CharacterBuildings/domain/CharacterBuildingPatch';
 import { CharacterBuildingRepository } from '../../../../../src/Contexts/apiApp/CharacterBuildings/domain/interfaces';
 import { BaseRepositoryMock } from '../../shared/__mocks__/BaseRepositoryMock';
 import { CharacterBuildingMother } from '../domain/mothers';
@@ -6,7 +8,7 @@ import { CharacterBuildingMother } from '../domain/mothers';
 export class CharacterBuildingRepositoryMock
   extends BaseRepositoryMock<
     CharacterBuilding,
-    CharacterBuilding,
+    CharacterBuildingPatch,
     { id: string }
   >
   implements CharacterBuildingRepository
@@ -27,5 +29,12 @@ export class CharacterBuildingRepositoryMock
     );
 
     return this.isFindable && !foundBuilds.length ? this.storage : foundBuilds;
+  }
+
+  async update(
+    characterBuilding: CharacterBuildingPatch,
+    username: Username
+  ): Promise<void> {
+    this.updateMock(characterBuilding, username);
   }
 }
