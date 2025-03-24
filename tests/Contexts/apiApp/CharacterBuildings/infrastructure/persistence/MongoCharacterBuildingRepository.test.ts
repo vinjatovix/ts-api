9a1495aff6a611c3e54ff2295431d615bb10e0ea
vinjatovix <vinjatovix@gmail.com>
2025-03-24 21:46:15 +0000

    [GH-210] feat(CharacterBuildings): implement CharacterBuildingPatcher and associated request interface for patching character buildings

--- tests/Contexts/apiApp/CharacterBuildings/infrastructure/persistence/MongoCharacterBuildingRepository.test.ts
+++ tests/Contexts/apiApp/CharacterBuildings/infrastructure/persistence/MongoCharacterBuildingRepository.test.ts
@@ -1,7 +1,12 @@
 import container from '../../../../../../src/apps/apiApp/dependency-injection';
 import { CharacterBuildingRepository } from '../../../../../../src/Contexts/apiApp/CharacterBuildings/domain/interfaces';
 import { EnvironmentArranger } from '../../../../shared/infrastructure/arranger/EnvironmentArranger';
+import { UserMother } from '../../../Auth/domain/mothers';
 import { CharacterBuildingMother } from '../../domain/mothers';
+import { CharacterBuildingPatch } from '../../../../../../src/Contexts/apiApp/CharacterBuildings/domain/CharacterBuildingPatch';
+import { SceneCircumstanceMother } from '../../../Scenes/domain/mothers';
+
+const username = UserMother.random().username;
 
 const environmentArranger: Promise<EnvironmentArranger> = container.get(
   'apiApp.EnvironmentArranger'
@@ -81,6 +86,30 @@ describe('MongoCharacterBuildingRepository', () => {
       expect(storedCharacterBuilding).not.toMatchObject({
         character: characterBuilding.character
       });
+    });
+  });
+
+  describe('update', () => {
+    it('should update an existing character building', async () => {
+      const sceneCircumstances = SceneCircumstanceMother.random().value;
+      const characterBuilding = CharacterBuildingMother.random();
+      await repository.save(characterBuilding);
+
+      const updatedCharacterBuilding = CharacterBuildingPatch.fromPrimitives({
+        id: characterBuilding.id.value,
+        sceneCircumstances
+      });
+
+      await repository.update(updatedCharacterBuilding, username);
+
+      expect(await repository.search(characterBuilding.id.value)).toMatchObject(
+        {
+          sceneCircumstances: expect.objectContaining({
+            value: sceneCircumstances
+          }),
+          metadata: { updatedBy: username.value }
+        }
+      );
     });
   });
 
